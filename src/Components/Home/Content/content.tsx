5ef52ec79635726abc903c3ea8e9f361e5701807
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-07-17 19:49:25 -0400

    Adding MapleStory character Animation for home page
    - Added 4 standing animation images to `public`
    - Modified the Content component to have non-empty right component
    - Implemented animation logic leveraging React's `useState` and `useEffect` hooks
    - Implemented animation logic to enforce a loop effect as seen in the
      game

--- src/Components/Home/Content/content.tsx
+++ src/Components/Home/Content/content.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './content.css';
 
 const Content: React.FC = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [direction, setDirection] = useState(1);
+  const imageNames = ['stand1_0.png', 'stand1_1.png', 'stand1_2.png', 'stand1_3.png'];
+  const totalImages = imageNames.length;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => {
+        let nextIndex = prevIndex + direction;
+
+        if (nextIndex >= totalImages) {
+          nextIndex = totalImages - 2;
+          setDirection(-1);
+        } else if (nextIndex < 0) {
+          nextIndex = 1;
+          setDirection(1);
+        }
+
+        return nextIndex;
+      });
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, [totalImages, direction]);
+
   return (
     <div className="ContentContainer">
       <div className="LeftContainer">
@@ -9,7 +34,7 @@ const Content: React.FC = () => {
         <p>I like building cool things.</p>
       </div>
       <div className="RightContainer">
-        {/* Add content for the RightContainer if needed */}
+        <img src={`${process.env.PUBLIC_URL}/${imageNames[currentIndex]}`} alt={`stand1_${currentIndex}.png`} width="200" />
       </div>
     </div>
   );
