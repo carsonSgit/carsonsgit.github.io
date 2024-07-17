fada590aed86755f0636fb304411a7d3aa084b3c
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-07-17 19:59:56 -0400

    Added Hover image differentiation
    - When hovering the animation, the image changes to a different sprite

--- src/Components/Home/Content/content.tsx
+++ src/Components/Home/Content/content.tsx
@@ -4,7 +4,9 @@ import './content.css';
 const Content: React.FC = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [direction, setDirection] = useState(1);
+  const [hoverIndex, setHoverIndex] = useState(0);
   const imageNames = ['stand1_0.png', 'stand1_1.png', 'stand1_2.png', 'stand1_3.png'];
+  const hoverImageNames = ['hover_stand1_0.png', 'hover_stand1_1.png', 'hover_stand1_2.png', 'hover_stand1_3.png'];
   const totalImages = imageNames.length;
 
   useEffect(() => {
@@ -22,10 +24,18 @@ const Content: React.FC = () => {
 
         return nextIndex;
       });
-    }, 500);
+    }, 500); // Interval time in milliseconds
 
-    return () => clearInterval(interval);
-  }, [totalImages, direction]);
+    return () => clearInterval(interval); // Cleanup interval on component unmount
+  }, [totalImages, direction]); // Dependencies for useEffect
+
+  const handleMouseEnter = (index: number) => {
+    setHoverIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoverIndex(0); // Reset to default image when not hovered
+  };
 
   return (
     <div className="ContentContainer">
@@ -34,7 +44,15 @@ const Content: React.FC = () => {
         <p>I like building cool things.</p>
       </div>
       <div className="RightContainer">
-        <img src={`${process.env.PUBLIC_URL}/${imageNames[currentIndex]}`} alt={`stand1_${currentIndex}.png`} width="200" />
+        <img
+          src={`${process.env.PUBLIC_URL}/${
+            hoverIndex !== 0 ? hoverImageNames[hoverIndex - 1] : imageNames[currentIndex]
+          }`}
+          alt={`stand1_${currentIndex}.png`}
+          onMouseEnter={() => handleMouseEnter(currentIndex + 1)}
+          onMouseLeave={handleMouseLeave}
+          width="200"
+        />
       </div>
     </div>
   );
