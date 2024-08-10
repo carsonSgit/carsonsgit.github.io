4ef92384b1751cfc295b521f3c58b85753f6bd96
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-08-10 13:26:03 -0400

    feat: Bad version of about page, gearing towards goal

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import './About.scss';
+import { FaPython, FaJava, FaJsSquare, FaReact, FaGitAlt, FaDocker } from 'react-icons/fa';
+import { SiCsharp, SiSqlite, SiKotlin, SiDotnet, SiXamarin, SiGooglecolab } from 'react-icons/si';
 
 const About: React.FC = () => {
   return (
     <div className="AboutContainer">
       <h1 className="AboutTitle">About Me</h1>
-      <div className="AboutContent">
-        <p>
-          Hi, I'm Carson! I'm a passionate software developer currently working as a Junior Software Engineer. 
-          I love building cool things and exploring the world of machine learning. I'm also an active participant in hackathons, 
-          constantly seeking opportunities to enhance my skills and make an impact.
-        </p>
-        <p>
-          In my free time, I work on indie game development, collaborate on open-source projects, 
-          and dive into Leetcode challenges to sharpen my problem-solving abilities. 
-          I believe in continuous learning and strive to stay up-to-date with the latest technologies.
-        </p>
-        <p>
-          My journey into software development began with a technical DEC in Computer Science from John Abbott College, 
-          and I'm now pursuing further studies in economics to meet the prerequisites for a BCompSc at Concordia University. 
-          My ultimate goal is to contribute to meaningful projects and grow as a developer.
-        </p>
+      <div className="AboutContentContainer">
+        <div className="AboutContent">
+          <p>
+            Hi, I'm Carson! I'm a passionate software developer currently working as a Junior Software Engineer. 
+            I love building cool things and exploring the world of machine learning. I'm also an active participant in hackathons, 
+            constantly seeking opportunities to enhance my skills and make an impact.
+          </p>
+        </div>
+
+        <div className="Technologies">
+          <h2 className="TechnologiesTitle">Technologies I Work With</h2>
+          <ul className="TechList">
+            <li><FaPython /> Python</li>
+            <li><FaJava /> Java</li>
+            <li><FaJsSquare /> JavaScript</li>
+            <li><SiCsharp /> C#</li>
+            <li><SiSqlite /> SQL</li>
+            <li><SiKotlin /> Kotlin</li>
+            <li><FaReact /> React</li>
+            <li><SiDotnet /> ASP.NET, .NET MAUI</li>
+            <li><SiXamarin /> Xamarin</li>
+            <li><FaGitAlt /> Git, GitHub</li>
+            <li><SiGooglecolab /> Google Colab</li>
+            <li><FaDocker /> Docker</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
