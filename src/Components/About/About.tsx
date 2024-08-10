af49fd03d8019128c02610396f03a23d4942a16b
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-08-10 17:48:12 -0400

    feat: Add crazy amounts of technologies

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import './About.scss';
-import { FaPython, FaJava, FaJsSquare, FaReact, FaGitAlt, FaDocker } from 'react-icons/fa';
-import { SiCsharp, SiSqlite, SiKotlin, SiDotnet, SiXamarin, SiGooglecolab } from 'react-icons/si';
+import {
+  FaPython, FaJava, FaJsSquare, FaReact, FaGitAlt, FaDocker, FaHtml5, FaCss3Alt, FaAws, FaNodeJs, FaSass, FaPhp, FaMarkdown,
+  FaAngular, FaBootstrap, FaFigma, FaRaspberryPi
+} from 'react-icons/fa';
+import {
+  SiCsharp, SiSqlite, SiKotlin, SiDotnet, SiXamarin, SiTypescript, SiJquery, SiNextdotjs, SiAzuredevops, SiFirebase, SiVercel,
+  SiCloudflare, SiExpress, SiTailwindcss, SiNginx, SiMongodb, SiMysql, SiMicrosoftsqlserver, SiCanva, SiGimp, SiKeras, SiPytorch, SiScikitlearn,
+  SiPostman, SiSwagger, SiJira, SiGradle, SiKubernetes, SiArduino, SiCisco, SiUnity
+} from 'react-icons/si';
 
 const About: React.FC = () => {
   return (
@@ -22,15 +29,53 @@ const About: React.FC = () => {
             <li><FaPython /> Python</li>
             <li><FaJava /> Java</li>
             <li><FaJsSquare /> JavaScript</li>
+            <li><SiTypescript /> TypeScript</li>
             <li><SiCsharp /> C#</li>
-            <li><SiSqlite /> SQL</li>
+            <li><SiSqlite /> SQLite</li>
             <li><SiKotlin /> Kotlin</li>
-            <li><FaReact /> React</li>
             <li><SiDotnet /> ASP.NET, .NET MAUI</li>
+            <li><FaReact /> React</li>
+            <li><SiNextdotjs /> Next.js</li>
+            <li><SiJquery /> jQuery</li>
             <li><SiXamarin /> Xamarin</li>
-            <li><FaGitAlt /> Git, GitHub</li>
-            <li><SiGooglecolab /> Google Colab</li>
+            <li><FaGitAlt /> Git</li>
             <li><FaDocker /> Docker</li>
+            <li><SiUnity /> Unity</li>
+            <li><SiAzuredevops /> Microsoft Azure</li>
+            <li><FaHtml5 /> HTML5</li>
+            <li><FaCss3Alt /> CSS3</li>
+            <li><FaMarkdown /> Markdown</li>
+            <li><FaPhp /> PHP</li>
+            <li>PowerShell</li>
+            <li><FaAws /> AWS</li>
+            <li><SiCloudflare /> Cloudflare</li>
+            <li><SiFirebase /> Firebase</li>
+            <li><SiVercel /> Vercel</li>
+            <li><FaAngular /> Angular</li>
+            <li><FaBootstrap /> Bootstrap</li>
+            <li><SiExpress /> Express.js</li>
+            <li>Mantine</li>
+            <li><FaNodeJs /> Node.js</li>
+            <li><FaSass /> SASS</li>
+            <li><SiTailwindcss /> TailwindCSS</li>
+            <li><SiNginx /> Nginx</li>
+            <li><SiMongodb /> MongoDB</li>
+            <li><SiMicrosoftsqlserver /> Microsoft SQL Server</li>
+            <li><SiMysql /> MySQL</li>
+            <li><SiCanva /> Canva</li>
+            <li><FaFigma /> Figma</li>
+            <li><SiGimp /> Gimp</li>
+            <li><SiKeras /> Keras</li>
+            <li><SiPytorch /> PyTorch</li>
+            <li><SiScikitlearn /> scikit-learn</li>
+            <li><SiPostman /> Postman</li>
+            <li><SiSwagger /> Swagger</li>
+            <li><SiJira /> Jira</li>
+            <li><SiGradle /> Gradle</li>
+            <li><SiKubernetes /> Kubernetes</li>
+            <li><SiArduino /> Arduino</li>
+            <li><SiCisco /> Cisco</li>
+            <li><FaRaspberryPi /> Raspberry Pi</li>
           </ul>
         </div>
       </div>
