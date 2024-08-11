8ed4610994f3242285040da91b23bf30c56d6706
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-08-10 22:21:47 -0400

    fix: Isolate hover effect

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -19,13 +19,13 @@ const About: React.FC = () => {
             Hi, my name is Carson! I'm a recent grad from John Abbott College's Computer Science degree continuing my education over at Concordia University!
             <br />
             <br />
-            This summer, I interned @ <a href="https://www.tailed.ca/en" target='_blank' rel="noreferrer">Tail'ed</a> as a Software Developer, working on the company website using TypeScript, React, and Next.js. Alongside that, I also worked on some tools for users written in Python (notably, an AI-tool).
+            This summer, I interned @ <a className="hoverLink" href="https://www.tailed.ca/en" target='_blank' rel="noreferrer">Tail'ed</a> as a Software Developer, working on the company website using TypeScript, React, and Next.js. Alongside that, I also worked on some tools for users written in Python (notably, an AI-tool).
             <br />
             <br />
             In my free time, I'm usually working on a personal project, learning a new technology, or wallowing in some imposter syndrome. Aside from that, my interests are geared towards AI/ML and their potential impact on environmental solutions.
             <br />
             <br />
-            I'm not hard to track down, but you can find me on <a href="www.linkedin.com/in/carsonspriggs" target="_blank" rel="noreferrer">LinkedIn</a>, <a href="https://github.com/carsonSgit" target='_blank' rel="noreferrer">GitHub</a>. I also dabble in a bit of writing on my <a href="https://dev.to/carsonsgit" target='_blank' rel="noreferrer">Dev.to</a> and <a href="https://medium.com/@carsonspriggs6" target='_blank' rel="noreferrer">Medium</a> pages.
+            I'm not hard to track down, but you can find me on <a className="hoverLink" href="www.linkedin.com/in/carsonspriggs" target="_blank" rel="noreferrer">LinkedIn</a>, <a className="hoverLink" href="https://github.com/carsonSgit" target='_blank' rel="noreferrer">GitHub</a>. I also dabble in a bit of writing on my <a className="hoverLink" href="https://dev.to/carsonsgit" target='_blank' rel="noreferrer">Dev.to</a> and <a className="hoverLink" href="https://medium.com/@carsonspriggs6" target='_blank' rel="noreferrer">Medium</a> pages.
           </p>
         </div>
 
