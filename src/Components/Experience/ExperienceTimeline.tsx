21afa3b146d8c909d0d24cc85eed5e0d16f3e81e
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-08-16 00:22:41 -0400

    feat: Better content in ExperienceTimeline

--- src/Components/Experience/ExperienceTimeline.tsx
+++ src/Components/Experience/ExperienceTimeline.tsx
@@ -2,34 +2,34 @@ import { motion } from 'framer-motion';
 import './ExperienceTimeline.scss';
 
 const ExperienceTimeline = () => (
-    <div className="timeline">
-      <motion.div
-        initial={{ opacity: 0, x: -50 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.5 }}
-        className="timeline-item"
-      >
-        <div className="timeline-date">June 2024 - Ongoing</div>
-        <div className="timeline-content">
-          <h3>Bachelor of Economics</h3>
-          <h4>Concordia University</h4>
-          <p>Getting necessary prerequisites before transfering to a Bachelors of Computer Science.</p>
-        </div>
-      </motion.div>
-      <motion.div
-        initial={{ opacity: 0, x: -50 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.5 }}
-        className="timeline-item"
-      >
-        <div className="timeline-date">March 2024 - July 2024</div>
-        <div className="timeline-content">
-          <h3>Director</h3>
-          <h4>JACHacks</h4>
-          <p>Getting necessary prerequisites before transfering to a Bachelors of Computer Science.</p>
-        </div>
-      </motion.div>
-    </div>
-  );
+  <div className="timeline">
+    <motion.div
+      initial={{ opacity: 0, x: -50 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.5 }}
+      className="timeline-item"
+    >
+      <div className="timeline-date">June 2024 - Ongoing</div>
+      <div className="timeline-content">
+        <h3>SDE Intern</h3>
+        <h4>Tail'ed</h4>
+        <p>Building the company website & developing an AI product.</p>
+      </div>
+    </motion.div>
+    <motion.div
+      initial={{ opacity: 0, x: -50 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.5 }}
+      className="timeline-item"
+    >
+      <div className="timeline-date">March 2024 - July 2024</div>
+      <div className="timeline-content">
+        <h3>Director</h3>
+        <h4>JACHacks</h4>
+        <p>Led the organization of the hackathon, overseeing event planning and execution.</p>
+      </div>
+    </motion.div>
+  </div>
+);
 
 export default ExperienceTimeline;
