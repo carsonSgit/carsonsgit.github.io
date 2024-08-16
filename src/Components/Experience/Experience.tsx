f3b3d3f36d719b90f8e4cc89b3aaecad5707a759
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-08-16 02:49:28 -0400

    feat: Styling and remove Education component

--- src/Components/Experience/Experience.tsx
+++ src/Components/Experience/Experience.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ExperienceTimeline from './ExperienceTimeline';
-import EducationTimeline from './EducationTimeline';
 import './Experience.scss';
 
 const Experience = () => (
   <div className="experience-page">
-    <h1>My Experience</h1>
     <ExperienceTimeline />
   </div>
 );
