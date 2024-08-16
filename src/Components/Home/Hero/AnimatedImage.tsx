1a26059750b461aae1fc9bf55c1b6a2fd9ee5507
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-08-16 04:20:32 -0400

    style: Code organization

--- src/Components/Home/Hero/AnimatedImage.tsx
+++ src/Components/Home/Hero/AnimatedImage.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { AnimatedImageProps } from '../../Interfaces/animatedImageProps';
 
 const AnimatedImage: React.FC<AnimatedImageProps> = ({
   imageNames,
