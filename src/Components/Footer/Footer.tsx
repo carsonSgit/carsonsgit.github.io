a8cee1491d779de5d87ada171f2c47b5a0496a6c
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-08-16 21:40:14 -0400

    style: Run Prettier on codebase

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -6,22 +6,32 @@ const Footer: React.FC = () => {
   return (
     <footer className="Footer">
       <div className="FooterIcons">
-        <a href="https://github.com/carsonSgit" target="_blank" rel="noreferrer">
+        <a
+          href="https://github.com/carsonSgit"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FaGithub />
         </a>
-        <a href="https://www.linkedin.com/in/carsonspriggs" target="_blank" rel="noreferrer">
+        <a
+          href="https://www.linkedin.com/in/carsonspriggs"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FaLinkedin />
         </a>
         <a href="https://dev.to/carsonsgit" target="_blank" rel="noreferrer">
           <FaDev />
         </a>
-        <a href="https://medium.com/@carsonspriggs6" target="_blank" rel="noreferrer">
+        <a
+          href="https://medium.com/@carsonspriggs6"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FaMedium />
         </a>
       </div>
-      <div className="FooterText">
-        Coded and Designed by @carsonSgit
-      </div>
+      <div className="FooterText">Coded and Designed by @carsonSgit</div>
     </footer>
   );
 };
