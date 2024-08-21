85c15188a4b936d50c64d6001d7b9336fdaac43e
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-08-21 01:07:22 -0400

    feat: Script to hit all public repos of mine and return the languages

--- src/Components/About/getGitHubStats.ts
+++ src/Components/About/getGitHubStats.ts
@@ -7,8 +7,16 @@ export const getGitHubProfileStats = async () => {
 }
 
 export const getGitHubProfileLanguages = async () => {
-    const response = await fetch('https://api.github.com/users/carsonSgit/repos');
-    const data = await response.json();
-    return data;
+    const reposResponse = await fetch('https://api.github.com/users/carsonSgit/repos');
+    const repos = await reposResponse.json();
 
+    const languagesPromises = repos.map(async (repo: { name: string }) => {
+        const languagesResponse = await fetch(`https://api.github.com/repos/carsonSgit/${repo.name}/languages`);
+        const languages = await languagesResponse.json();
+        return { repo: repo.name, languages };
+    });
+
+    const languagesData = await Promise.all(languagesPromises);
+    
+    return languagesData;
 }
