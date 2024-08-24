97683f6269b26b97662c042ef68c87666033c4a5
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-08-23 21:26:55 -0400

    chore: Tweak project description

--- src/Components/Data/portfolioProjects.ts
+++ src/Components/Data/portfolioProjects.ts
@@ -2,15 +2,6 @@ import { Project } from '../Interfaces/projects';
 
 export const projects: Project[] = [
   {
-    image: 'DEADWEIGHT.png',
-    title: 'DEADWEIGHT',
-    languages: 'C#, Unity',
-    description:
-      'DEADWEIGHT is a game developed for the Kenney Jam 2024. It was my first experience working in Unity done in collaboration with <a href="https://github.com/NoahGJAC" target="_blank" rel="noopener noreferrer">NoahGJAC</a> as the first project for our game studio, <a href="https://github.com/Cano-Studios" target="_blank" rel="noopener noreferrer">Cano Studios</a>.',
-    github: 'https://github.com/Cano-Studios/DEADWEIGHT',
-    website: 'https://cano-studios.itch.io/deadweight',
-  },
-  {
     image: 'cropcare.png',
     title: 'CropCare',
     languages: 'Python, .NET MAUI, Raspberry Pi, IoT',
@@ -36,5 +27,14 @@ export const projects: Project[] = [
       'TradeMind is a project developed for <a href="https://launchlab.ai/" target="_blank" rel="noopener noreferrer">AI Launch Lab</a>\'s <a href="https://launchlab.ai/rd-program/" target="_blank" rel="noopener noreferrer">R&D Program</a>. It is a web app that uses machine learning to predict stock prices and provide insights to users.',
     github: 'https://github.com/carsonSgit/TradeMind',
     website: 'https://trademind.pages.dev/',
+  },  
+  {
+    image: 'DEADWEIGHT.png',
+    title: 'Mice Neural Decoding',
+    languages: 'Python, Jupyter Notebook, Machine Learning',
+    description:
+      'Mice Neural Decoding was a challenge at the <a href="https://pharmahacks.com/#welcome" target="_blank" rel="noopener noreferrer">PharmaHacks 2024</a> event. My group and I decoded mouse navigation decisions by analyzing L2/3 neuron activity in the retrosplenial cortex, following the findings of Tseng et al. (2022) in their Neuron Paper. Please read the README.md on the GitHub repo!',
+    github: 'https://github.com/carsonSgit/Mice-Neural-Decoding-ML',
+    website: 'https://github.com/carsonSgit/Mice-Neural-Decoding-ML/blob/main/PharmaHacks%202024%20Neural%20Decoding%20Single%20File.ipynb',
   },
 ];
