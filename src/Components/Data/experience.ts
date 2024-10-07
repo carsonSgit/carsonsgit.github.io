0ee669bc2335922de0985e76455044531fd73e57
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-10-07 10:30:17 -0400

    chore: Convert Experience Descriptions into an array

--- src/Components/Data/experience.ts
+++ src/Components/Data/experience.ts
@@ -1,18 +1,23 @@
 export const experience = [
   {
-    date: 'May 2024 - Ongoing',
+    date: 'May 2024 - August 2024',
     title: 'SDE Intern',
     company: "Tail'ed",
-    description: 'Building the company website & developing an AI product.',
+    description: [
+      'Worked on the company website using Next.js and React.',
+      'Developed an AI model to rank user skills in regards to a job position based on their CV.'
+    ],
     statsKey: 'SDE Intern',
     link: 'https://tailed.ca',
   },
   {
     date: 'March 2024 - Ongoing',
-    title: 'HD Montreal',
+    title: 'HD Montreal & Sponsors',
     company: 'CUSEC',
-    description:
+    description: [
       'Leading the delegation of post-secondary students across Montreal for the annual conference.',
+      'Negotiating sponsorships with companies to secure their sponsorship of the NPO conference.'
+    ],
     statsKey: 'Head Delegate Montreal',
     link: 'https://2024.cusec.net/',
   },
@@ -20,8 +25,11 @@ export const experience = [
     date: 'January 2024 - May 2024',
     title: 'IT Intern',
     company: 'Town of Kirkland',
-    description:
-      'Delivered technical support to government employees and trained under the Cybersecurity Lead of the municipality.',
+    description: [
+      'Delivered technical support to government employees across 5 municipal buildings.',
+      'Analysed and resolved 4500+ Cybersecurity vulnerabilities in our network using CrowdStrike Falcon.',
+      'Set up, maintained, and managed 100+ workstations to ensure employee productivity.'
+    ],
     statsKey: 'IT Intern',
     link: 'https://www.ville.kirkland.qc.ca/',
   },
@@ -29,8 +37,10 @@ export const experience = [
     date: 'March 2024 - July 2024',
     title: 'Director',
     company: 'JACHacks',
-    description:
-      'Led the organization of the hackathon, overseeing event planning and execution.',
+    description: [
+      'Managed a team of 5 to ensure the successful organization and execution of the hackathon.',
+      'Organized the event with a short timeline of 1.5 months, resulting in 100+ participants and overly positive feedback.'
+    ],
     statsKey: 'Director',
     link: 'https://jachacks.pages.dev/',
   },
@@ -38,8 +48,12 @@ export const experience = [
     date: 'February 2024 - April 2024',
     title: 'R&D Fellowship',
     company: 'AI Launch Lab',
-    description:
-      'Learned under PhDs while developing TradeMind for my fellowship project.',
+    description: [
+      'Learned Machine Learning and AI topics and material under PhDs.',
+      'Developed the ML project TradeMind to predict the stock market using Python and Keras.',
+      'Presented the project to a panel of investors and industry professionals.',
+      'Noted as the top project of the program\'s conception.'
+    ],
     statsKey: 'R&D Fellowship',
     link: 'https://launchlab.ai/rd-program/',
   },
