1a8781cddf46f89108cc99944d34f0439b611512
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-12-30 23:13:11 -0500

    style: Improvements to current progression

--- src/Components/Projects/ProjectItem.tsx
+++ src/Components/Projects/ProjectItem.tsx
@@ -1,32 +1,37 @@
 import React from 'react';
-import { useInView } from 'react-intersection-observer';
 import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
-import { motion } from 'framer-motion';
 import { ProjectItemProps } from '../Interfaces/projectItemProps';
 import './Projects.scss';
 
 const ProjectItem: React.FC<ProjectItemProps> = ({ project }) => {
-  const [ref, inView] = useInView({
-    triggerOnce: true,
-    threshold: 0.1,
-  });
-
   return (
-    <motion.div
-      className="ProjectItem"
-      initial={{ scale: 1 }}
-      animate={{
-        scale: inView ? 1 : 0.95,
-      }}
-      transition={{ duration: 0.6 }}
-      ref={ref}
-      whileHover={{
-        scale: 1.02,
-        transition: { duration: 0.1, ease: 'linear' },
-      }}
-    >
+    <div className="ProjectItem">
       <div className="ProjectContent">
-        <h2 className="ProjectTitle">{project.title}</h2>
+        <div className="ProjectTitleContainer">
+          <h2 className="ProjectTitle">{project.title}</h2>
+          <div className="ProjectLinks">
+            {project.github && (
+              <a
+                href={project.github}
+                className="ProjectLink"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub />
+              </a>
+            )}
+            {project.website && (
+              <a
+                href={project.website}
+                className="ProjectLink"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaExternalLinkAlt />
+              </a>
+            )}
+          </div>
+        </div>
         <div className="ProjectLanguages">
           {project.languages.map((language, index) => (
             <span
@@ -45,30 +50,8 @@ const ProjectItem: React.FC<ProjectItemProps> = ({ project }) => {
           className="ProjectDescription"
           dangerouslySetInnerHTML={{ __html: project.description }}
         />
-        <div className="ProjectLinks">
-          {project.github && (
-            <a
-              href={project.github}
-              className="ProjectLink"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaGithub />
-            </a>
-          )}
-          {project.website && (
-            <a
-              href={project.website}
-              className="ProjectLink"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaExternalLinkAlt />
-            </a>
-          )}
-        </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
