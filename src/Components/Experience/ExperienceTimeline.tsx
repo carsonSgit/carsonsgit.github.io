8ee977061b635b1e58ab5807966b940e61417ac3
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-12-31 01:39:24 -0500

    style: Timeline hover effect

--- src/Components/Experience/ExperienceTimeline.tsx
+++ src/Components/Experience/ExperienceTimeline.tsx
@@ -1,35 +1,19 @@
-import { motion, useAnimation } from 'framer-motion';
-import { useEffect } from 'react';
 import { education } from '../Data/education';
 import { experience } from '../Data/experience';
 import { statistics } from '../Data/statistics';
 import './ExperienceTimeline.scss';
 
 const ExperienceTimeline = () => {
-  const controls = useAnimation();
-
-  useEffect(() => {
-    controls.start(i => ({
-      opacity: 1,
-      y: 0,
-      transition: { delay: i * 0.2 },
-    }));
-  }, [controls]);
 
   return (
     <div className="experience-grid">
       <div className="timeline">
-        <motion.div className="particle-background" />
 
         <div className="timeline-section">
           <h2 className="timeline-section-title">Education</h2>
           {education.map((item, index) => (
-            <motion.div
+            <div
               key={index}
-              custom={index}
-              initial={{ opacity: 0, y: 20 }}
-              animate={controls}
-              exit={{ opacity: 0, y: 20 }}
               className="timeline-item"
             >
               <a
@@ -39,9 +23,10 @@ const ExperienceTimeline = () => {
                 className="timeline-link"
               >
                 <div className="timeline-step education-step"></div>
+
                 <div className="timeline-content">
                   <h3>
-                    <span className="timeline-role">{item.title}</span> <span className="timeline-atsign">@ </span>
+                    <span className="timeline-role">{item.title}</span> <span className="timeline-atsign">• </span>
                     <span className="timeline-institution">{item.institution}</span>
                   </h3>
                   <div className="timeline-date">{item.date}</div>
@@ -58,19 +43,13 @@ const ExperienceTimeline = () => {
                   </div>
                 </div>
               </a>
-            </motion.div>
+            </div>
           ))}
         </div>
-
         <div className="timeline-section">
-          <h2 className="timeline-section-title">Experience</h2>
           {experience.map((item, index) => (
-            <motion.div
+            <div
               key={index}
-              custom={index + education.length}
-              initial={{ opacity: 0, y: 20 }}
-              animate={controls}
-              exit={{ opacity: 0, y: 20 }}
               className="timeline-item"
             >
               <a
@@ -80,9 +59,10 @@ const ExperienceTimeline = () => {
                 className="timeline-link"
               >
                 <div className="timeline-step experience-step"></div>
+
                 <div className="timeline-content">
                   <h3>
-                    <span className="timeline-role">{item.title}</span> <span className="timeline-atsign">@ </span>
+                    <span className="timeline-role">{item.title}</span> <span className="timeline-atsign">• </span>
                     <span className="timeline-company">{item.company}</span>
                   </h3>
                   <div className="timeline-date">{item.date}</div>
@@ -103,7 +83,7 @@ const ExperienceTimeline = () => {
                   </div>
                 </div>
               </a>
-            </motion.div>
+            </div>
           ))}
         </div>
       </div>
