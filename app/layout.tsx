5a87cf128c0d3971e440c79b95f014be478d13d7
carsonSgit <carsonspriggs6@gmail.com>
2026-02-27 14:52:26 -0500

    speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.scss";
 import Script from "next/script";
 
@@ -128,6 +129,7 @@ export default function RootLayout({
 					strategy="afterInteractive"
 					defer
 				/>
+				<SpeedInsights />
 			</body>
 		</html>
 	);
